3226ed056063a1aa53deb8e9fd5070f7ac43fbe1
unibreakfast <unibreakfast1@gmail.com>
2020-05-17 11:25:13 +0300

    blank item list class

---
+++ ItemList.js
@@ -0,0 +1,5 @@
+class ItemList {
+  constructor (templateObj, options) {
+
+  }
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+templateObj = {name: 'unnamed', age: null, gender: null}
+
+options = {}
+
+myList = new ItemList(templateObj, options)
